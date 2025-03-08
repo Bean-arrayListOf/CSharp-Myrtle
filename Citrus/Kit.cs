5b687817199743f66847d436a00628538f6b5fed
Bean-arrayListOf <arraylistof@163.com>
2025-03-08 09:40:06 +0800

    删除不必要的冗余

--- Citrus/Kit.cs
+++ Citrus/Kit.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -10,16 +9,12 @@ public static class Kit
 {
     public static readonly OSType Platform = getOSType();
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void Out(this object? any) => Console.Write(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void OutLine(this object? any) => Console.WriteLine(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void Write(this object? any) => Console.Write(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void WriteLine(this object? any) => Console.WriteLine(any);
 
     public static TextReader ReaderOf(string file)
@@ -46,10 +41,11 @@ public static class Kit
     {
         var lines = new List<String>();
         var read = new StreamReader(stream);
-        while(read.ReadLine() is { } line)
+        while (read.ReadLine() is { } line)
         {
             lines.Add(line);
         }
+
         return lines;
     }
 
@@ -63,51 +59,39 @@ public static class Kit
             {
                 baos.Write(bytes, 0, n);
             }
+
             baos.Flush();
             return baos.ToArray();
         }
     }
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static byte[] EncBytes(this string hex) => Encoding.Default.GetBytes(hex);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static string EncString(this byte[] bytes) => Encoding.Default.GetString(bytes);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static ResourceManager ResourceOf(string baseName) => new ResourceManager(baseName, typeof(Kit).Assembly);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public static ResourceManager GetResource(this Assembly baseAssembly, string baseName) => new ResourceManager(baseName, baseAssembly);
+    public static ResourceManager GetResource(this Assembly baseAssembly, string baseName) =>
+        new ResourceManager(baseName, baseAssembly);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static bool ToBoolean(this object? any) => Convert.ToBoolean(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static short ToShort(this object? any) => Convert.ToInt16(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static ushort ToUShort(this object? any) => Convert.ToUInt16(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static int ToInt(this object? any) => Convert.ToInt32(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static uint ToUInt(this object? any) => Convert.ToUInt32(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static long ToLong(this object? any) => Convert.ToInt64(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static ulong ToULong(this object? any) => Convert.ToUInt64(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static string? ToStrings(this object? any) => Convert.ToString(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static double ToDouble(this object? any) => Convert.ToDouble(any);
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static float ToFloat(this object? any) => Convert.ToSingle(any);
 
     public static OSType getOSType()
@@ -126,7 +110,7 @@ public static class Kit
         {
             return OSType.Windows;
         }
-        
+
         if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
         {
             return OSType.FreeBSD;
@@ -135,7 +119,7 @@ public static class Kit
         throw new Exception("未知系统");
     }
 
-    public static ResourceManager GetResource(this Type baseType,string baseName)
+    public static ResourceManager GetResource(this Type baseType, string baseName)
     {
         return new ResourceManager(baseName, baseType.Assembly);
     }
