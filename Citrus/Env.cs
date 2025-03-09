024ba5dfbfdedff419d671c1c95c808efa5c6db4
Bean-arrayListOf <arraylistof@163.com>
2025-03-09 17:59:55 +0800

    修复Env

--- Citrus/Env.cs
+++ Citrus/Env.cs
@@ -6,6 +6,7 @@ namespace CSharp_Myrtle.Citrus;
 public static class Env
 {
     public static readonly ResourceManager cr = Assembly.GetExecutingAssembly().GetResource("CSharp_Myrtle.MasterResource");
+    public static readonly string TempPath = Path.GetTempPath();
     public static string? Get(string key)
     {
         return Environment.GetEnvironmentVariable(key);
