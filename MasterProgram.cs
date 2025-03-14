805be99c09d56097f5fd8bf6c9e7c1fb7a93c78c
Bean-arrayListOf <arraylistof@163.com>
2025-03-15 06:43:25 +0800

    6

--- MasterProgram.cs
+++ MasterProgram.cs
@@ -8,7 +8,7 @@ namespace CSharp_Myrtle
         {
             Kit.PrintTable(["1", "2", "3"], [["我", "m1.1", "m1.2"], ["m2", "我", "m2.2"], ["m3", "m3.1", "我"]])
                 .OutLine();
-            // 5
+            // 6
         }
     }
 }
