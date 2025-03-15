4a1b280cd4e1f10b83b69d9dc5fde1552a022256
Bean-arrayListOf <arraylistof@163.com>
2025-03-15 09:30:17 +0800

    测试时间戳

--- MasterProgram.cs
+++ MasterProgram.cs
@@ -1,9 +1,17 @@
-﻿namespace CSharp_Myrtle
+﻿using CSharp_Myrtle.Citrus;
+
+namespace CSharp_Myrtle
 {
     internal class MasterProgram
     {
         static void Main(string[] args)
         {
+            var start = Kit.TimeSeconds();
+
+
+            var end = Kit.TimeSeconds();
+
+            Console.WriteLine("{0}MS", (end - start));
         }
     }
 }
