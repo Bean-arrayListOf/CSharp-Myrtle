6f8d6664cd74084fadfc31c1ee8006fdc8a61eb2
Bean-arrayListOf <arraylistof@163.com>
2025-03-15 08:36:22 +0800

    优化Hash，修复部分Bug

--- Citrus/Hash.cs
+++ Citrus/Hash.cs
@@ -4,61 +4,68 @@ namespace CSharp_Myrtle.Citrus;
 
 public class Hash
 {
-	public static byte[] Get(HashMode mode, Stream data)
-	{
-		HashAlgorithm sha;
-		switch (mode)
-		{
-			case HashMode.SHA1:
-				{
-					sha = SHA1.Create();
-					break;
-				}
-			case HashMode.SHA3_256:
-				{
-					sha = SHA3_256.Create();
-					break;
-				}
-			case HashMode.SHA3_384:
-				{
-					sha = SHA3_384.Create();
-					break;
-				}
-			case HashMode.SHA3_512:
-				{
-					sha = SHA3_512.Create();
-					break;
-				}
-			case HashMode.SHA256:
-				{
-					sha = SHA256.Create();
-					break;
-				}
-			case HashMode.SHA384:
-				{
-					sha = SHA384.Create();
-					break;
-				}
-			case HashMode.SHA512:
-				{
-					sha = SHA512.Create();
-					break;
-				}
-			default:
-				{
-					sha = SHA256.Create();
-					break;
-				}
-		}
+    public static byte[] Get(HashMode? mode, Stream data)
+    {
+        using var sha = GetSHA(mode);
+        return sha.ComputeHash(data);
+    }
 
-		var cache = sha.ComputeHash(data);
-		sha.Dispose();
-		return cache;
-	}
+    public static byte[] Get(HashMode? mode, byte[] data)
+    {
+        using var sha = GetSHA(mode);
+        return sha.ComputeHash(data);
+    }
 
-	public static byte[] Get(HashMode mode, byte[] data)
-	{
-		using var ms = new MemoryStream(data);
-		return Get(mode, ms);
-	}
+    public static byte[] Get(HashMode? mode, string data) => Get(mode, data.EncBytes());
+
+    public static string GetToHex(HashMode? mode, byte[] data) => ToHex(Get(mode, data));
+
+    public static string GetToHex(HashMode? mode, Stream data) => ToHex(Get(mode, data));
+
+    public static string GetToHex(HashMode? mode, string data) => GetToHex(mode, data.EncBytes());
+
+    public static string GetToBase64(HashMode? mode, byte[] data) => ToBase64(Get(mode, data));
+
+    public static string GetToBase64(HashMode? mode, Stream data) => ToBase64(Get(mode, data));
+
+    public static string GetToBase64(HashMode? mode, string data) => ToBase64(Get(mode, data));
+
+    public static HashAlgorithm GetSHA(HashMode? mode)
+    {
+        if (mode == null)
+        {
+            return GetDefaultSHA();
+        }
+
+        return mode switch
+        {
+            HashMode.SHA1 => SHA1.Create(),
+            HashMode.SHA256 => SHA256.Create(),
+            HashMode.SHA384 => SHA384.Create(),
+            HashMode.SHA512 => SHA512.Create(),
+            HashMode.SHA3_256 => SHA3_256.Create(),
+            HashMode.SHA3_384 => SHA3_384.Create(),
+            HashMode.SHA3_512 => SHA3_512.Create(),
+            _ => GetDefaultSHA()
+        };
+    }
+
+    public static HashAlgorithm GetDefaultSHA()
+    {
+        return (HashMode)Env.cr.GetString("sha")!.ToInt() switch
+        {
+            HashMode.SHA1 => SHA1.Create(),
+            HashMode.SHA256 => SHA256.Create(),
+            HashMode.SHA384 => SHA384.Create(),
+            HashMode.SHA512 => SHA512.Create(),
+            HashMode.SHA3_256 => SHA3_256.Create(),
+            HashMode.SHA3_384 => SHA3_384.Create(),
+            HashMode.SHA3_512 => SHA3_512.Create(),
+            _ => SHA256.Create()
+        };
+    }
+
+    static string ToHex(byte[] data) => data.HexString();
+
+    static string ToBase64(byte[] data) => Convert.ToBase64String(data);
 }
