50e2ad14e7ca8e51db00dc382e77fe67ec364a39
Bean-arrayListOf-Fork <arraylistof@163.com>
2025-03-17 11:44:54 +0800

    修改命名规范

---
+++ Citrus/ICacheAllocator.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Myrtle.Citrus
+{
+    interface ICacheAllocator : IDisposable
+    {
+        public string GetRoot();
+        public (Stream stream, string uuid) CreateStream();
+        public (string file, string uuid) CopyFile(string inputFile);
+        public (string file, string uuid)? Get(string uuid);
+        public List<string> GetKeys();
+        public (string file, string uuid) CreateFile(string inputFile);
+    }
+}
